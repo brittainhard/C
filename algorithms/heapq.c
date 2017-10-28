1f2124e58cd7ea379629a89b32c4b2394c37a707
Brittain Hard <brittainhard@gmail.com>
2017-10-28 13:11:28 -0500

    Makefile uses garbage collector.

--- algorithms/heapq.c
+++ algorithms/heapq.c
@@ -11,7 +11,19 @@ typedef struct Node {
 } Node;
 
 
+Node *new_node(int priority)
+{
+    Node *new_node = gmalloc(sizeof(Node));
+    new_node->priority = priority;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
 int main(int argc, char *argv[])
 {
+    Node *head = new_node(1);
+    printf("%d\n", head->priority);
+    collect_garbage();
     return 0;
 }
