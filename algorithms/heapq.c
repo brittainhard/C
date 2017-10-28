9368eedb9829e6331359b8664fad2121bbaf59b3
Brittain Hard <brittainhard@gmail.com>
2017-10-28 13:01:16 -0500

    Algorithms folder.

---
+++ algorithms/heapq.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../garbage/garbage.h"
+
+
+typedef struct Node {
+    int priority;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
